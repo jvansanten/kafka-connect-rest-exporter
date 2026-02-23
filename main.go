2f88acd000027d7ea894c1906d11f7253fe4754e
Jakob van Santen <jvansanten@gmail.com>
2026-02-23 15:51:44 +0100

    working collector

---
+++ main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"resty.dev/v3"
+)
+
+type collectorsResponse []string
+
+type collectorOffsetsResponse struct {
+	Offsets []struct {
+		Partition struct {
+			Cluster   string `json:"cluster"`
+			Partition int    `json:"partition"`
+			Topic     string `json:"topic"`
+		} `json:"partition"`
+		Offset struct {
+			Offset int `json:"offset"`
+		} `json:"offset"`
+	} `json:"offsets"`
+}
+
+type restAPICollector struct {
+	offset *prometheus.Desc
+
+	api *resty.Client
+}
+
+func newRestAPICollector(api_url string) prometheus.Collector {
+
+	return &restAPICollector{
+		offset: prometheus.NewDesc("kafka_connect_current_offset",
+			"The current offset of the Kafka Connect connector",
+			[]string{"connector", "cluster", "topic", "partition"}, nil,
+		),
+		api: resty.New().SetBaseURL(api_url),
+	}
+}
+
+// Each and every collector must implement the Describe function.
+// It essentially writes all descriptors to the prometheus desc channel.
+func (collector *restAPICollector) Describe(ch chan<- *prometheus.Desc) {
+
+	//Update this section with the each metric you create for a given collector
+	ch <- collector.offset
+}
+
+// Collect implements required collect function for all promehteus collectors
+func (collector *restAPICollector) Collect(ch chan<- prometheus.Metric) {
+
+	response, err := collector.api.R().SetResult(&collectorsResponse{}).Get("/connectors")
+	if err != nil {
+		log.Printf("Error fetching connectors: %v", err)
+		return
+	}
+	for _, connector := range *response.Result().(*collectorsResponse) {
+		log.Printf("Connector: %s", connector)
+
+		response, err := collector.api.R().SetResult(&collectorOffsetsResponse{}).Get(fmt.Sprintf("/connectors/%s/offsets", connector))
+		if err != nil {
+			log.Printf("Error fetching offsets for connector %s: %v", connector, err)
+			continue
+		}
+		for _, offset := range response.Result().(*collectorOffsetsResponse).Offsets {
+			ch <- prometheus.MustNewConstMetric(
+				collector.offset,
+				prometheus.GaugeValue,
+				float64(offset.Offset.Offset),
+				connector,
+				offset.Partition.Cluster,
+				offset.Partition.Topic,
+				fmt.Sprintf("%d", offset.Partition.Partition),
+			)
+		}
+	}
+}
+
+func main() {
+	connectAPI, port, err := parseArgs(os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+
+	_ = connectAPI
+
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+		newRestAPICollector(connectAPI),
+	)
+
+	http.Handle("/metrics", handlers.LoggingHandler(os.Stdout, promhttp.HandlerFor(reg, promhttp.HandlerOpts{})))
+	log.Printf("Connect API at %v", connectAPI)
+	log.Printf("Starting server on port %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func parseArgs(args []string) (string, int, error) {
+	fs := flag.NewFlagSet("kafka-connect-rest-exporter", flag.ContinueOnError)
+	// https://github.com/prometheus/prometheus/wiki/Default-port-allocations
+	// says that 9840 is the default port for Kafka Connect exporter, so we will
+	// use it as default here as well.
+	port := fs.Int("port", 9840, "Port to bind the metrics server to")
+
+	if err := fs.Parse(args); err != nil {
+		return "", 0, err
+	}
+
+	remaining := fs.Args()
+	if len(remaining) < 1 {
+		return "", 0, fmt.Errorf("missing required argument: connect-api")
+	}
+
+	if len(remaining) > 1 {
+		return "", 0, fmt.Errorf("unexpected extra arguments: %v", remaining[1:])
+	}
+
+	return remaining[0], *port, nil
+}
